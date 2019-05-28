0ebc48d63a4b926487bc7767ac14d1c0aeabbf27
viraloverhead <viral.overhead@gmail.com>
2019-05-29 00:57:32 +0200

    test

--- Il Tempio Di Elettra/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-/*
-No es tan complicado, solo deben importar el JS con el idioma español y despues le indican al datepicker que usaran ese idioma.
-*/
-
-$('#datepicker').datepicker({
-  uiLibrary: 'bootstrap4',
-  locale: 'es-es',
-});
